fe07487496136efbd3336a980e764c3cdd6f518e
colorlessenergy <brianmunozdev@gmail.com>
2019-12-25 13:50:57 -0800

    Add some customizations

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
-test('renders learn react link', () => {
+test('renders the custom text', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+
+  const linkElement = getByText(/Hi my name is Brian!/i);
   expect(linkElement).toBeInTheDocument();
 });
